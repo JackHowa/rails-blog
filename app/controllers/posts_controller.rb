d762a48f55ded490d7343dfca82cd60b76b5c2b6
jackhowa <johncarverhoward@gmail.com>
2017-06-18 12:20:21 -0500

    add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,26 @@
 class PostsController < ApplicationController
 	# rails generated via $ rails g controller posts 
-	def index
-		# connected via the config/routes
+
+	def index # connected via the config/routes
+		@posts = Post.all.order("created_at DESC")
 	end
-	def new
+
+	def new # Makes a new form 
 	end
+
+	def create # Post route for Post
+		@post = Post.new(post_params)
+		@post.save
+
+		redirect_to @post
+	end 
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	private 
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end 
 end
